7614d2dfef10d86a20baa9a50d73be08a53ec9fa
dennx2 <jdcht1228@gmail.com>
2023-12-05 14:17:57 -0500

    add put, delete, and get routes for individual genre

--- index.js
+++ index.js
@@ -27,6 +27,38 @@ app.post('/api/genres', (req, res) => {
   res.send(genre);
 });
 
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
+});
+
+
 const validateGenre = genre => {
   const schema = Joi.object({
     name: Joi.string().min(3).required()
