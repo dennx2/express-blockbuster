3ae62f71c2c351044d20d3cbd060291fba7208e4
dennx2 <jdcht1228@gmail.com>
2023-12-14 17:58:32 -0500

    Add connection to MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/streaming')
+  .then(console.log('Connected to MongoDB...'))
+  .catch(console.log('Could not connect to MongoDB...'));
+
 
 app.use(express.json());
 app.use('/api/genres', genres);
