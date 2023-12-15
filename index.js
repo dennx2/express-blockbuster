ba91bfa719cd2a414001e90511d50b5a397faccf
dennx2 <jdcht1228@gmail.com>
2023-12-14 20:48:07 -0500

    Create customer schemas and apis

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
@@ -10,9 +11,9 @@ mongoose
   .then(console.log('Connected to MongoDB...'))
   .catch(console.log('Could not connect to MongoDB...'));
 
-
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers/', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
