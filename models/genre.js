792cc9c173f3a80fa7255f12045042a8509d34a0
dennx2 <jdcht1228@gmail.com>
2023-12-16 21:43:46 -0500

    Create movies schema and routes

--- models/genre.js
+++ models/genre.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Genre = mongoose.model(
-  'Genre',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-    }
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 const validateGenre = genre => {
   const schema = Joi.object({
@@ -21,4 +20,6 @@ const validateGenre = genre => {
 };
 
 module.exports.Genre = Genre;
+module.exports.genreSchema = genreSchema;
 module.exports.validate = validateGenre;
+
