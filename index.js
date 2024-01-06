72875a84a9e8b9c9ddd30e9b229e7aa03b5e611c
dennx2 <jdcht1228@gmail.com>
2024-01-06 14:24:41 -0500

    Extract routes

--- index.js
+++ index.js
@@ -5,16 +5,10 @@ require('express-async-errors');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
 const express = require('express');
 const app = express();
 
+require('./startup/routes')(app);
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERRPR: jwtPrivateKey is not defined.');
@@ -34,15 +28,9 @@ mongoose
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.log('Could not connect to MongoDB...', err));
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
 
-app.use(error);
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
